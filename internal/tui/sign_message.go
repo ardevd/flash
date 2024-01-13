6e07ae263908183c582bab20a38c3ec1fd7e83d9
ardevd <ardevd@protonmail.com>
2024-01-13 12:30:47 +0100

    started implementing the message signing component

---
+++ internal/tui/sign_message.go
@@ -0,0 +1,98 @@
+package tui
+
+import (
+	"context"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/lightninglabs/lndclient"
+	"github.com/lightningnetwork/lnd/keychain"
+)
+
+// Model for the message signign view
+type SignMessageModel struct {
+	styles     *Styles
+	lndService *lndclient.GrpcLndServices
+	ctx        context.Context
+	base       *BaseModel
+	keys       keyMap
+	form       *huh.Form
+}
+
+// Value container
+var messageToSign string
+
+func newSignMessageModel(service *lndclient.GrpcLndServices, base *BaseModel) *SignMessageModel {
+	m := SignMessageModel{lndService: service, base: base, ctx: context.Background(), keys: Keymap}
+	m.styles = GetDefaultStyles()
+	m.base.pushView(&m)
+	m.form = getMessageSigningForm()
+
+	return &m
+}
+
+// Model Update logic
+func (m *SignMessageModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// Handle Base model logic
+	model, cmd := m.base.Update(msg)
+	if model != nil {
+		return model, cmd
+	}
+
+	var cmds []tea.Cmd
+
+	// Process the form
+	if m.form != nil {
+		form, cmd := m.form.Update(msg)
+		if f, ok := form.(*huh.Form); ok {
+			m.form = f
+			cmds = append(cmds, cmd)
+		}
+
+		if m.form.State == huh.StateCompleted {
+			if len(messageToSign) > 0 {
+				m.lndService.Signer.SignMessage()
+			}
+		}
+	}
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m SignMessageModel) signMessage() {
+	keyLocator := &keychain.KeyLocator{
+		Family: 6,
+		Index:  0,
+	}
+
+	// Call the SignMessage function
+	signature, err := m.lndService.Signer.SignMessage(m.ctx, lnrpc.SignMessageRequest{
+		Msg:     msgBytes,
+		Locator: keyLocator,
+	})
+}
+
+func getMessageSigningForm() *huh.Form {
+	form := huh.NewForm(
+		huh.NewGroup(huh.NewNote().
+			Title("Sign Message").
+			Description("Sign a message with your node's private key"),
+			huh.NewInput().
+				Title("Message to sign").
+				Prompt("?").
+				Value(&messageToSign)))
+	form.NextField()
+	return form
+}
+
+func (m SignMessageModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m SignMessageModel) View() string {
+	v := strings.TrimSuffix(m.form.View(), "\n\n")
+	form := lipgloss.DefaultRenderer().NewStyle().Margin(1, 0).Render(v)
+	return lipgloss.JoinVertical(lipgloss.Left, form)
+}
